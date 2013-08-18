3b42dd249b2b3111231f917741c884a734a37856
Bhanu Pratap Chaudhary <bhanuc@iitk.ac.in>
2013-08-17 23:43:26 -0700

    embedded policy to push username into project model

--- jutja.com/api/policies/set_owner.js
+++ jutja.com/api/policies/set_owner.js
@@ -1,8 +1,9 @@
 module.exports = function(req, res, next) {
     if (req.session.user) {
       var action = req.param('action');
+	var cuser = req.user.username ;// cuser is the loggedin user
       if (action == 'create') {
-	req.body.createdby = req.user.username;
+	req.body.owner.push (cuser);
 	  }	
     next();
     } else {
