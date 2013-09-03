78441fe1b4209c926a52754769a486eee9d36e0a
Gladon Almeida <gladon.almeida@gmail.com>
2013-09-02 22:48:48 -0700

    Added backbone

---
+++ prototype/js/views/views.js
@@ -0,0 +1,203 @@
+// Function to call template for cleaner code
+window.template = function (id) {
+	return _.template($('#' + id).html());
+};
+
+App.Views.NodeGraph = Backbone.View.extend({
+	initialize : function(){
+		this.constants = {
+			NODE_HEIGHT : 30,
+			SVG_HEIGHT : 500,
+			SVG_WIDTH : 960
+		};
+		this.render();
+	},
+	openNodeContextMenu : function(data, index){
+		  	d3.select('#node-context-menu')
+		  	.style('position', 'absolute')
+		  	.style('left', d3.event.layerX + "px")
+		  	.style('top', d3.event.layerY + "px")
+		  	.style('display', 'block');
+		  	d3.event.preventDefault();
+		  },
+	update : function () {
+		var nodes = this.flatten(this.root),
+			vis = this.vis,	
+			links = d3.layout.tree().links(nodes),
+			that = this;
+
+	  	// Restart the force layout.
+	  	this.force.nodes(nodes)
+	  	.links(links)
+	  	.start()
+	  	.linkStrength(3);
+
+
+	  	// Update the links…
+	  	this.link = vis.selectAll("line.link")
+	  	.data(links, function(d) { return d.target.id; });
+
+	  	// Enter any new links.
+	  	this.link.enter().insert("svg:line", ".node")
+	  	.attr("class", "link")
+	  	.attr("x1", function(d) { return d.source.x; })
+	  	.attr("y1", function(d) { return d.source.y; })
+	  	.attr("x2", function(d) { return d.target.x; })
+	  	.attr("y2", function(d) { return d.target.y; });
+
+	  	// Exit any old links.
+	  	this.link.exit().remove();
+	  	// Update the nodes…
+	  	this.node = vis.selectAll("g.node")
+	  	.data(nodes, function(d) { return d.id; })
+	  	.enter()
+	  	.append("g")
+	  	.attr("class", "node")
+	  	.on("dbclick", this.selectNode)
+	  	.on("click",this.addChild)
+	  	.on("contextmenu",this.openNodeContextMenu);
+
+
+	  	this.node.append("svg:rect")
+	  	.attr("width", function(d) { return d.size/2 })
+	  	.attr("height", that.constants.NODE_HEIGHT)
+	  	.attr("x", function(d) { return d.x-(d.size/4); })
+	  	.attr("y", function(d) { return d.y-that.constants.NODE_HEIGHT/2; })
+	  	.style("fill", this.color);
+
+
+	  	this.rect = vis.selectAll(".node rect");
+	  	
+	  	this.node.append("text")
+	  	.attr("dx", 12)
+	  	.attr("dy", ".35em")
+	  	.text(function(d) { return d.name })
+	  	.attr("x", function(d) { return d.x; })
+	  	.attr("y", function(d) { return d.y; })
+	  	.attr("class","title")
+	  	.attr("text-anchor","middle");
+
+	  	this.text = vis.selectAll(".node text.title");
+
+
+	  	this.node.call(this.force.drag);
+	},
+	// Returns a list of all nodes under the root.
+	flatten : function(root) {
+		var nodes = [], i = 0;
+
+		function recurse(node) {
+			if (node.children) node.children.forEach(recurse);
+			if (!node.id) node.id = ++i;
+			nodes.push(node);
+		}
+
+		recurse(root);
+		return nodes;
+	},
+	// Color leaf nodes orange, and packages white or blue.
+	color : function (d) {
+		return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+	},
+
+	// Toggle children on click.
+	click : function(d) {
+		if (d.children) {
+			d._children = d.children;
+			d.children = null;
+		} else {
+			d.children = d._children;
+			d._children = null;
+		}
+		this.update();
+	},
+
+	selectNode : function(d){
+		var currentRect = d3.select(this).select('rect');
+		rect.style('stroke','')
+		.style('stroke-width','');
+		currentRect.style('stroke','red')
+		.style('stroke-width','2px');
+	},
+
+	render : function(){
+		var node,
+		link,
+		root,
+		rect,
+		text,
+		force,
+		that = this;
+
+		this.node = node;
+		
+		this.link = link;
+		this.root = root;
+		this.rect = rect;
+		this.text = text;
+
+		function tick() {
+			that.link.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; });
+
+			that.rect.attr("x", function(d) {
+				d.x = Math.min(that.constants.SVG_WIDTH, d.x);
+				return d.x-(d.size/4);
+			})
+			.attr("y", function(d) {
+				d.y = Math.min(that.constants.SVG_HEIGHT, d.y);
+				return d.y-that.constants.NODE_HEIGHT/2;
+			}); 
+
+			that.text.attr("x", function(d) {return d.x; })
+			.attr("y", function(d) { return d.y; })
+		}
+
+		force = d3.layout.force()
+		.on("tick", tick)
+		.size([this.constants.SVG_WIDTH, this.constants.SVG_HEIGHT]);
+
+		this.force = force;
+
+		force.linkDistance(function(link,index){
+			return link.source.size/5+link.target.size/5+50;
+		});
+
+		force.charge(function(node,index){
+			return -(node.size*5);
+		}).friction(0.4)
+		.gravity(0.01);
+
+		var vis = d3.select(this.el).append("svg:svg")
+		.attr("width", this.constants.SVG_WIDTH)
+		.attr("height", this.constants.SVG_HEIGHT);
+
+		this.vis = vis;
+
+		d3.json("readme.json", function(json) {
+			that.root = json;
+			that.update();
+		});
+
+		$('#node-context-menu').mouseleave(function(e){
+			$(this).hide();
+		});
+
+		this.addChild = function (d){
+	  		if(!d.children){
+	  			d.children = [];
+	  		}
+	  		d.children.push({
+	  			name : "test",
+	  			size : d.size * 0.8,
+	  			x: d.x,
+	  			y: d.y
+	  		});
+	  		that.update();
+	  	}
+
+		return this;
+	}
+});
---
+++ prototype_0/js/app.js
@@ -0,0 +1,138 @@
+(function () {
+
+	function ColorLuminance(hex, lum) {
+		// validate hex string
+		hex = String(hex).replace(/[^0-9a-f]/gi, '');
+		if (hex.length < 6) {
+			hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+		}
+		lum = lum || 0;
+		// convert to decimal and change luminosity
+		var rgb = "#", c, i;
+		for (i = 0; i < 3; i++) {
+			c = parseInt(hex.substr(i*2,2), 16);
+			c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+			rgb += ("00"+c).substr(c.length);
+		}
+		return rgb;
+	}
+
+	var Node = function(data) {
+		this.initialize(data);
+	}
+		var p = Node.prototype = new createjs.Container(); // inherit from Container
+
+		p.label;
+		p.background;
+		p.count = 0;
+
+		p.Container_initialize = p.initialize;
+		p.initialize = function(data) {
+			this.Container_initialize();
+			this.label = data.label;
+
+			if (!data.color){ 
+				this.color = "#CCC"; 
+			}else{
+				this.color = data.color;
+			}
+
+			
+			var nodeLabel = new createjs.Text(this.label, "14px Arial", "#000");
+			nodeLabel.textBaseline = "top";
+			nodeLabel.textAlign = "left";
+
+			var nodeDescription = new createjs.Text(data.description, "11px Arial", "#000");
+			nodeDescription.textBaseline = "top";
+			nodeDescription.textAlign = "left";
+			nodeDescription.x = 10;
+			nodeDescription.y = 25;
+
+			var plusMinus = new createjs.Text('+', "18px Arial", "#000");
+			plusMinus.textBaseline = "center";
+
+			
+			// var width = nodeLabel.getMeasuredWidth()+40;
+			// var height = nodeLabel.getMeasuredHeight()+20;
+
+			this.width = 150;
+			this.height = 40;
+
+			//debugger;
+			plusMinus.x = this.width-20;
+			plusMinus.y = 20;
+
+			
+			this.background = new createjs.Shape();
+			this.background.graphics.beginFill(this.color).drawRect(0,0,this.width,this.height,10);
+			
+			nodeLabel.x = 10;
+			nodeLabel.y = 10;
+			
+			this.addChild(this.background,nodeLabel,plusMinus);
+			this.addChild(nodeDescription);
+
+			plusMinus.addEventListener('click',function(e){
+				if(e.target.text==='+')
+					e.target.text = '-';
+				else
+					e.target.text = '+';
+				e.target.getStage().update();
+			});
+			//this.addEventListener("click", this.handleClick);  
+			//this.addEventListener("tick", this.handleTick);
+
+			var childX = -200, 
+			childY = 80;
+
+			for(childIndex in data.children){
+
+				data.children[childIndex].color = ColorLuminance(this.color,0.8);
+				
+				var childNode = new Node(data.children[childIndex]);
+				childNode.x = childX;
+				childNode.y = childY;
+				childX += childNode.width+20;
+				this.addChild(childNode);
+			}
+		}
+
+
+		p.handleClick = function (event) {    
+			var target = event.target;
+			alert("You clicked on a Node: "+target.label);
+		} 
+
+
+
+		window.Node = Node;
+	}());
+
+
+	var stage, holder;
+	function init() {
+		window.stage = new createjs.Stage("demoCanvas");
+		var data = {
+			label: "Product",
+			color: "#62E200",
+			description: "This is the description",
+			children: [{
+				label: "task1",
+				description: "This is the description 1",
+				children: [{
+					label: "task11",
+					description: "This is the description 1"
+				},{
+					label: "task12",
+					description: "This is the description 1"
+				}]
+			},{
+				label: "task2",
+				description: "This is the description 2"
+			}]
+		}
+		window.node1 = stage.addChild(new Node(data));
+		node1.x = (stage.canvas.width/2)-(node1.width/2);
+		node1.y = 20;
+		stage.update();
+	}
