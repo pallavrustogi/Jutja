36410f42433b58ef9fb81bdbd62ea192404a7737
bhanuc <bhanuc@iitk.ac.in>
2013-10-20 16:07:24 -0700

    added some routes

--- latest_console/routes.js
+++ latest_console/routes.js
@@ -1,37 +1,27 @@
 'use strict';
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.set('X-Auth-Required', 'true');
+
+
+exports = module.exports = function(app, passport) {
+	
+	app.get("/dashboard", function(req, res) {
+	if(req.isAuthenticated()) { res.render('dashboard') }
+	else {  res.set('X-Auth-Required', 'true');
   res.redirect('/login/?returnUrl='+ encodeURIComponent(req.originalUrl));
 }
 
-function ensureAdmin(req, res, next) {
-  if (req.user.canPlayRoleOf('admin')) {
-    return next();
-  }
-  res.redirect('/');
-}
-
-function ensureAccount(req, res, next) {
-  if (req.user.canPlayRoleOf('account')) {
-    if (req.app.get('require-account-verification')) {
-      if (req.user.roles.account.isVerified !== 'yes' && !/^\/account\/verification\//.test(req.url)) {
-        return res.redirect('/account/verification/');
-      }
-    }
-    return next();
-  }
-  res.redirect('/');
-}
-
-exports = module.exports = function(app, passport) {
- 
-  //route not found
-  app.all('*', function(req, res){
+}); 
+app.get('/login', function(req, res){
+	res.render('login');
+});
+app.get('/', function(req, res){
 	res.render('index');
 });
+  //route not found
+  app.all('*', function(req, res){
+	res.render('404');
+});
+
+
 };
 
