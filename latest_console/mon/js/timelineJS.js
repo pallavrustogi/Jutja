6fd649e49568e3ddfc7d31723aab22e40bc4f761
nidhijain128 <nidhiprj@usc.edu>
2014-01-16 15:21:50 -0800

    timeline.html

--- latest_console/mon/js/timelineJS.js
+++ latest_console/mon/js/timelineJS.js
@@ -3,9 +3,26 @@ function myFunction()
 			document.getElementById('col1').style.cssText = 'display:inherit !important;';
 		}
 		
+function deleteEntry(i)
+{
+	var entryname = 'entry' + i;
+	document.getElementById(entryname).innerHTML = "";
+}
+
+function editEntry(i)
+{
+	var entryname = 'entry' + i;
+	var tasktype = prompt("Enter type of task");
+	var taskdetails = prompt("Enter details of the task");
+	var tasktime = prompt("Enter deadline for the task");
+	var details = document.getElementById(entryname).innerHTML;
+	details = "<div class='task'><span><span class='type'>" + tasktype + "<a href='#' onclick=deleteEntry('" + i + "')><img src='images/delete.png' height='10px' width='10px' align='right'></a><a href='#' onclick=editEntry('" + i + "')><img src='images/edit.png' height='10px' width='10px' align='right'></a></span><span class='details'>" + taskdetails + "</span><span>remaining time<span class='remaining'>3h 38m 15s</span></span></span><div class='arrow'></div></div><div><input type='button' class='icon1' id='iconMin3' onclick=''/></div><div class='time'>" + tasktime;
+	document.getElementById(entryname).innerHTML = details;
+}
+		
 $(document).ready(function() {	
-var i=10;
-$("#iconMin21").hide();
+var i=1;
+/*$("#iconMin21").hide();
 $("#iconMin22").hide();
 $("#iconMin11").click(function() {
 	$("#col11").css("display","inherit");
@@ -26,23 +43,26 @@ $("#iconMin22").click(function() {
 	$("#col12").css("display","none");
 	$("#iconMin12").show();
 	$("#iconMin22").hide();
-	});
+	*/
 	
 $("#addEntry").click(function() {
+	var tasktype = prompt("Enter type of task");
+	var taskdetails = prompt("Enter details of the task");
+	var tasktime = prompt("Enter deadline for the task");
 	var allEntries = document.getElementById("allProjects").innerHTML;
 	if(i%2 != 0)
 	{
-		allEntries = allEntries + "<div class='timeslot'><div class='task'><span><span class='type'>Digital Ocean </span><span class='details'>Finish article on geddy</span><span>remaining time<span class='remaining'>3h 38m 15s</span></span></span><div class='arrow'></div></div><div><input type='button' class='icon1' id='iconMin3' onclick=''/></div><div class='time'>3:43 PM</div></div>";
+		allEntries = allEntries + "<div class='timeslot' id='entry" + i + "'><div class='task'><span><span class='type'>" + tasktype + "<a href='#' onclick=deleteEntry('" + i + "')><img src='images/delete.png' height='10px' width='10px' align='right'></a><a href='#' onclick=editEntry('" + i + "')><img src='images/edit.png' height='10px' width='10px' align='right'></a></span><span class='details'>" + taskdetails + "</span><span>remaining time<span class='remaining'>3h 38m 15s</span></span></span><div class='arrow'></div></div><div><input type='button' class='icon1' id='iconMin3' onclick=''/></div><div class='time'>" + tasktime + "</div></div>";
 		document.getElementById("allProjects").innerHTML = allEntries;
 	}
 	else
 	{	
-		allEntries = allEntries + "<div class='timeslot alt'><div class='task'><span><span class='type'>Digital Ocean </span><span class='details'>Finish article on geddy</span><span>remaining time<span class='remaining'>3h 38m 15s</span></span></span><div class='arrow'></div></div><div><input type='button' class='icon1' id='iconMin3' onclick=''/></div><div class='time'>3:43 PM</div></div>";
+		allEntries = allEntries + "<div class='timeslot alt' id='entry" + i + "'><div class='task'><span><span class='type'>" + tasktype + "<a href='#' onclick=deleteEntry('" + i + "')><img src='images/delete.png' height='10px' width='10px' align='right'></a><a href='#' onclick=editEntry('" + i + "')><img src='images/edit.png' height='10px' width='10px' align='right'></a></span><span class='details'>" + taskdetails + "</span><span>remaining time<span class='remaining'>3h 38m 15s</span></span></span><div class='arrow'></div></div><div><input type='button' class='icon1' id='iconMin3' onclick=''/></div><div class='time'>" + tasktime + "</div></div>";
 		document.getElementById("allProjects").innerHTML = allEntries;
 	}
 	i = i + 1;
 	
-	$("#iconMin11").click(function() {
+	/*$("#iconMin11").click(function() {
 		$("#col11").css("display","inherit");
 		$("#iconMin21").show();
 		$("#iconMin11").hide();
@@ -60,7 +80,6 @@ $("#addEntry").click(function() {
 	$("#iconMin22").click(function() {
 		$("#col12").css("display","none");
 		$("#iconMin12").show();
-		$("#iconMin22").hide();
-		});
+		$("#iconMin22").hide();*/
 });
 });
